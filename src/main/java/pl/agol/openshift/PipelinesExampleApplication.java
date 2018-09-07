9c885be0f5678b67789911abe64a0f40d15bef8a
agolawski <A.Golawski@polish-airports.com>
2018-09-07 08:25:51 +0200

    create simple example

---
+++ src/main/java/pl/agol/openshift/PipelinesExampleApplication.java
@@ -0,0 +1,12 @@
+package pl.agol.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinesExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelinesExampleApplication.class, args);
+    }
+}
